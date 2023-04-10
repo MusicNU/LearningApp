d340f0c02882a4d9bce8d5b1dc95d63f9b13692f
Siddharth Saha <siddharthsaha@dhcp-10-105-216-205.wireless.northwestern.private>
2023-04-10 18:23:23 -0500

    added info to homepage

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -4,11 +4,12 @@ import Button from '@mui/material/Button';
 export default function ButtonComponent() {
   return (
     <Box component="span" sx={{ p: 2, 
-                                border: '1px dashed grey'}}>
-      <Button sx = {{marginLeft:'auto',
-                    marginRight:'auto',
-                    width: '25%',
-                    textAlign: 'center'}}>Get Started</Button>
+                                border: '1px dashed grey',
+                                'position':'relative',
+                                'left':'37.5%'}}>
+      <Button sx = {{width: '25%',
+                    textAlign: 'center',
+                    }}>Get Started</Button>
     </Box>
   );
 }
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,11 +1,15 @@
 import { Box, Container, Grid, Typography, TextField} from '@mui/material/';
 import ButtonComponent from '../Components/Button';
+import TextBox from '../Components/TextCard';
+
 const HomePage = () => {
+    const headingText = "MusicNU"
+    const width = '25%'
+    const bodyText = 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.'
     return (
         <>
-            <p>
-                Welcome to MusicNU!
-            </p>
+            <TextBox>{headingText}</TextBox>
+            <TextBox>{bodyText}</TextBox>
             <ButtonComponent></ButtonComponent>
         </>
 
