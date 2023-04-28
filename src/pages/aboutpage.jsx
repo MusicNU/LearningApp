87472b5363ab4183d6fe7394978ea00c1572a257
Natalie Cheng <nataliecheng2026@u.northwestern.edu>
2023-04-28 17:57:27 -0500

    link tiilt website

--- src/pages/aboutpage.jsx
+++ src/pages/aboutpage.jsx
@@ -7,13 +7,16 @@ import { ReactComponent as Logo } from '../logo-multi.svg';
 
 const AboutPage = () => {
     const heading = "About"
-    const aboutText = "testing a long paragraph with words about tiilt maybe also a link to the tiilt website? also the tiilt website has been giving me a cert warning lately idk if its just me more words with words for testing about tiilt: tiilt is cool! with coolr people and cool projects"
+    const aboutText = "testing a long paragraph with words about tiilt (image links to tiilt website, but also add link in text?) also the tiilt website has been giving me a cert warning idk if its just me more words with words for testing about tiilt: tiilt is cool! with coolr people and cool projects"
 
     return(
     <>
         <ThemeProvider theme={theme}>
             <TextBox name={heading} width='50%' left='25%' fontSize='50px' marginTop='25px' elevation='0'></TextBox>
-            <div style={{position: 'absolute'}}><Logo width='100%' marginTop='25px'/></div>
+            <div style={{position: 'absolute'}}>
+                <a href="https://tiilt.northwestern.edu/" target="_blank" rel="noreferrer">
+                    <Logo width='100%' marginTop='25px'/></a>
+            </div>
             <TextBox name={aboutText} width='30%' left='50%' fontSize='20px' elevation='0'></TextBox>
             <TextBox name={'Meet Us!'} width='50%' left='25%' fontSize='50px' marginTop='70px' elevation='0'></TextBox>
         </ThemeProvider>
