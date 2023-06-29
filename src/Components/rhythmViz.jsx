3e3c6c4344242a8e21e71cb8e90ba5b3a3cba9a3
Siddharth Saha <siddharthsaha@Siddharths-MacBook-Air-2.local>
2023-06-29 15:56:58 +0530

    rhythm viz continued

--- src/Components/rhythmViz.jsx
+++ src/Components/rhythmViz.jsx
@@ -5,8 +5,10 @@ import { useTheme} from '@mui/material/styles';
   const Circ = (props) => {  
     return (
 
-    <svg className = "RhythCirc" height="100" width="100">
-      <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill={props.color} />
+    <svg className = "RhythCirc" width="200" height="200" viewBox="0 0 210 210">
+      <circle cx="100" cy="100" r="90" stroke-width="3" fill={props.color} />
+      <circle cx="100" cy="100" r="60" fill="#ADD8E6" />
+      <circle cx="100" cy="100" r="20" fill="#7393B3" />
     </svg>
     )
   }
