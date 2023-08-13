3bc52228e70098a269fadab6af81f2bca5b745cd
Siddharth Saha <siddharthsaha03@gmail.com>
2023-08-13 22:21:08 +0200

    bug fix

--- src/Components/pianoroll/musictracker.jsx
+++ src/Components/pianoroll/musictracker.jsx
@@ -1,31 +1,12 @@
-import React, { Component } from 'react'
-  
-class Tracker extends Component {
-  constructor(){
-    super()
-    this.state = {Position : 0}
-    this.makeTimer()
-  }
+import React, { Component } from 'react';
 
-  makeTimer(){
-    setInterval(() => {
+const Tracker = (props) => {
 
-        this.setState((state)=> {
-          if (state.Position > 610)
-            return {Position: 0};
-          else
-            return {Position: state.Position + 40};
-        })
-    }, 1000)
-  }
-
-
-  render()
-  {
     return (
-        <path d = {"M " + this.state.Position + " 150 l 0 -130 l -6 -6 l 0 -15 l 14 0 l 0 15 l -6 6 l 0 150 Z"} class="playhead"></path>
-    )
-  }
-}
+      <>
+        <path d = {"M " + props.displacement + " 150 l 0 -130 l -6 -6 l 0 -15 l 14 0 l 0 15 l -6 6 l 0 150 Z"} class="playhead"></path>
+      </>
+    )}
+
   
 export default Tracker
