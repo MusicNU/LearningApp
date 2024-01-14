6fc5d93074056e2f44c1316480b3e059e4007a5f
Ubuntu <timothyfu2023@u.northwestern.edu>
2024-01-14 22:10:04 +0000

    Updated Backend Code

--- server/audio_proc.py
+++ server/audio_proc.py
@@ -2,21 +2,22 @@ import sys, os
 sys.path.append(os.path.join(sys.path[0],'melody'))
 print(os.path.join(sys.path[0],'melody'))
 from rhythmID import rhythm_output
+from melodia import melody
+from flask import Flask, request
 
-
-from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/rhythm', methods=['GET'])
+
+@app.route('/rhythm', methods=['POST'])
 def spectro_output():
-    return {"beats":rhythm_output()}
+   return {"link": request.get_json(),"beats":rhythm_output(request.get_json())}
 
 
-@app.route("/melody")
+@app.route('/melody', methods=['POST'])
 def get_melody():
-    melody = "melody"
-    return {"melody": melody}
+   return {"link": request.get_json(),"melody":melody(request.get_json())}
 
 if __name__ == "__main__":
-    app.run(port = 6030, debug=True)
+   app.run(port = 6030, debug=True)
+
