3ce6661d8f84ecaebfdfd39f58612151861d03ab
Natalie Cheng <nataliecheng2026@u.northwestern.edu>
2024-01-21 17:08:29 -0600

    working audio player

--- src/pages/song.jsx
+++ src/pages/song.jsx
@@ -1,22 +1,28 @@
 import theme from '../theme';
 import ProgressBar from '../Components/ProgressBar';
-import MusicPlayerSlider from '../Components/MusicPlayer';
+import MusicPlayer from '../Components/MusicPlayer';
 import { Button, ThemeProvider } from '@mui/material';
 import NavButton from '../Components/NavButton';
+import ReactAudioPlayer from 'react-audio-player';
+
+const filename = "./hello.mp3"
+
+import audio_file from {filename}
 
 const SongPage = () => {
     return(
         <>
-        <div class='center'>
+        <div className='center'>
             <NavButton addr='/musicvis' label='Melody'></NavButton>
             <NavButton addr='song/chord' label='Chord'></NavButton>
             <NavButton addr='song/rhythm' label='Rhythm'></NavButton>
         </div>
-        <div class='center'>
-            <ProgressBar progress={70} length={120}/>
-        </div>
-        <div class='center'>
-            <MusicPlayerSlider/>
+        <div className='center'>
+            <ReactAudioPlayer
+                src={audio_file}
+                autoPlay={false}
+                controls
+            />
         </div>
         </>
     )
