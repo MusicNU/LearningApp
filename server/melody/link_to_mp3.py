39ee183a80fffb1217e0c62c2a23dcb595dac0d4
nawu1552 <nawu1552@gmail.com>
2024-01-26 13:39:22 -0600

    Updated code

--- server/melody/link_to_mp3.py
+++ server/melody/link_to_mp3.py
@@ -1,32 +1,7 @@
-# import os
-# import subprocess
-# from pytube import YouTube
-
-# link = ("https://www.youtube.com/watch?v=QLpArkPb-YU")
-
-# def link_to_mp3(song):
-#      # Use Pytube to convert link to mp4
-#     yt = YouTube(song)
-#     audio_stream = yt.streams.filter(only_audio=True).first()
-#     # audio_stream.download(output_path='mirex05/{}'.format(audio_stream.title))
-
-#     default_filename=audio_stream.title
-#     print("This is", default_filename)
-
-#     audio_stream.download(output_path='mirex05/{}'.format(default_filename))
-
-#     subprocess.run([
-#         'ffmpeg',
-#         '-i', os.path.join('mirex05/', default_filename + '.mp4'),
-#         os.path.join('mirex05/', default_filename + '.mp3')
-#     ])
-
-# link_to_mp3(link)
-
-
 import os
 import subprocess
 from pytube import YouTube
+import pytube
 
 link = "https://www.youtube.com/watch?v=QLpArkPb-YU"
 
@@ -46,7 +21,12 @@ def link_to_mp3(song):
     subprocess.run([
         'ffmpeg',
         '-i', os.path.join(output_folder, default_filename + '.mp4'),
-        os.path.join(output_folder, default_filename + '.mp3')
+        os.path.join(output_folder, pytube.extract.video_id(song) + '.mp3')
     ])
 
+    mp4_file = default_filename + '.mp4'
+
+    if os.path.exists(os.path.join(output_folder, mp4_file)):
+        os.remove(os.path.join(output_folder, mp4_file))
+   
 link_to_mp3(link)
--- server/melody/melodia.py
+++ server/melody/melodia.py
@@ -1,44 +1,22 @@
 from __future__ import print_function
-
 import vamp
 import librosa
 import matplotlib
 import matplotlib.pyplot as plt
 #just for plotting
 import numpy as np
+import numpy
 #what's wrong?
 # matplotlib.use('tkagg')
 # %matplotlib inline
 
 import IPython.display as ipd 
-import numpy as np 
 import io
 import json
 import requests
 from pytube import YouTube
 import os
 import subprocess
-
-#tutorial copied from : https://github.com/justinsalamon/melodia_python_tutorial/blob/master/melodia_python_tutorial.ipynb
-
-# You can download it here: http://labrosa.ee.columbia.edu/projects/melody/mirex05TrainFiles.zip
-
-
-# # This is how we load audio using Essentia
-# loader = es.MonoLoader(filename=audio_file, downmix = 'mix', sampleRate = 44100)
-# audio = loader()
-
-# This is how we load audio using Librosa
-# audio, sr = librosa.load(audio_file, sr=44100, mono=True)
-
-# data = vamp.collect(audio, sr, "mtg-melodia:melodia")
-
-
-# #hello
-# hop = 0.1
-# hop, melody = data['vector']
-# print(hop) # hop is currently 0.002902494
-# # print(melody)
 
 class NumpyArrayEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -48,24 +26,9 @@ class NumpyArrayEncoder(json.JSONEncoder):
     
 # TO DO: account for duplicate songs
 # TO DO: make data nicer for frontend to see, hop is currently 0.002902494
-def melody(song):
-    # Use Pytube to convert link to mp4
-    yt = YouTube(song["link"])
-    audio_stream = yt.streams.filter(only_audio=True).first()
-
-    default_filename=audio_stream.title
-    print("This is", default_filename)
-
-    audio_stream.download(output_path='mirex05/{}'.format(default_filename))
-
-    subprocess.run([
-        'ffmpeg',
-        '-i', os.path.join('mirex05/', default_filename),
-        os.path.join('mirex05/', default_filename + '.mp3')
-    ])
-
+def melody(video_id):
     # This is how we load audio using Librosa
-    audio, sr = librosa.load('mirex05/{}/{}'.format(audio_stream.title,audio_stream.title + '.mp4'), sr=44100, mono=True)
+    audio, sr = librosa.load('mirex05/{}'.format(video_id + '.mp3'), sr=44100, mono=True)
     params = {"minfqr": 100.0, "maxfqr": 800.0, "voicing": 0.2, "minpeaksalience": 0.0}
 
     data = vamp.collect(audio, sr, "mtg-melodia:melodia", parameters=params)
@@ -79,3 +42,5 @@ def melody(song):
     numpyData = {"notes": notes}
     encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)  # use dump() to write array into file
     return encodedNumpyData
+
+melody("QLpArkPb-YU")
