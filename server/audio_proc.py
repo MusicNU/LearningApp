e0d410a4c9810b6593874e28ca6564ed3da59212
Ubuntu <timothyfu2023@u.northwestern.edu>
2024-02-18 22:41:03 +0000

    working frontend to backend - resolved cors

--- server/audio_proc.py
+++ server/audio_proc.py
@@ -5,18 +5,20 @@ from rhythmID import rhythm_output
 from melodia import melody
 from link_to_mp3 import link_to_mp3
 from flask import Flask, request
-
+from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
-
+CORS(app)  # Enable CORS on the Flask app
 
 @app.route('/rhythm', methods=['POST'])
 def spectro_output():
+   # return {"result": "we can run"}
    return {"link": request.get_json(),"beats":rhythm_output(link_to_mp3(request.get_json()["link"]))}
 
 
 @app.route('/melody', methods=['POST'])
 def get_melody():
+   # return {"result": "we can run"}
    return {"link": request.get_json(),"melody":melody(link_to_mp3(request.get_json()["link"]))}
 
 if __name__ == "__main__":
--- src/Components/videos/videoDetail.jsx
+++ src/Components/videos/videoDetail.jsx
@@ -1,15 +1,40 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const VideoDetail = ({ video }) => {
-  // to add something before video add an if for !video
-  if (video) {
+  useEffect(() => {
+    if (video) {
+      const videoSrc = `https://www.youtube.com/watch?v=${video.id.videoId}`;
+      console.log(videoSrc);
 
-  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
-  console.log(typeof video);
+      const jsonString = '{ "link": "' + videoSrc + '"}';
+      const jsonObject = JSON.parse(jsonString);
+      console.log(jsonObject);
+
+      console.log("before api call");
+
+      axios.post('http://localhost:6030/melody', jsonObject)
+        .then(response => {
+          console.log("API call successful:", response.data);
+          // Handle response data if needed
+        })
+        .catch(error => {
+          console.error("Error making API call:", error);
+          // Handle error if needed
+        })
+        .finally(() => {
+          console.log("after api call");
+        });
+    }
+  }, [video]);
+
+  if (!video) return null;
+
   return (
     <div>
       <div className="center">
-        <iframe src={videoSrc} allowFullScreen title="Video player" className="video"/>
+        {/* <iframe src={videoSrc} allowFullScreen title="Video player" className="video"/> */}
       </div>
       <div className="ui segment">
         <h3 className="ui header">{video.snippet.title}</h3>
@@ -17,8 +42,6 @@ const VideoDetail = ({ video }) => {
       </div>
     </div>
   );
-
-  }
 };
 
 export default VideoDetail;
