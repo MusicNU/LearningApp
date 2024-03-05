b49c9df26a1dfbb2e37fc72098ecf528fd3e2edf
Ubuntu <timothyfu2023@u.northwestern.edu>
2024-03-05 09:45:03 +0000

    melodia now saves to s3

--- server/melody/melodia.py
+++ server/melody/melodia.py
@@ -1,22 +1,15 @@
 from __future__ import print_function
 import vamp
 import librosa
-import matplotlib
-import matplotlib.pyplot as plt
-#just for plotting
 import numpy as np
 import numpy
-#what's wrong?
-# matplotlib.use('tkagg')
-# %matplotlib inline
-
-import IPython.display as ipd 
 import io
 import json
 import requests
 from pytube import YouTube
 import os
 import subprocess
+import boto3 #aws
 
 class NumpyArrayEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -24,6 +17,10 @@ class NumpyArrayEncoder(json.JSONEncoder):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
     
+# note: must run with sudo permissions 
+s3 = boto3.client('s3')
+bucket_name = 'musicnu'
+
 # TO DO: account for duplicate songs
 # TO DO: make data nicer for frontend to see, hop is currently 0.002902494
 def melody(video_id):
@@ -40,8 +37,7 @@ def melody(video_id):
     clean_mel = [note for note in mel_array if note > 0]
     notes = librosa.hz_to_note(clean_mel)
     numpyData = {"notes": notes}
+    file_name = video_id + "_melody" + ".json"
     encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)  # use dump() to write array into file
-    # print(encodedNumpyData)
+    s3.put_object(Bucket=bucket_name, Key=file_name, Body=encodedNumpyData) #adds to the s3 bucket
     return encodedNumpyData
-
-# melody('QLpArkPb-YU')
