5b91bcd9c7e4d1a854468a71dca18cc0ba563446
JustinAronson <justinaronson@gmail.com>
2024-04-16 17:58:23 -0500

    added time calc

--- server/backend/rnn_beat_processor.py
+++ server/backend/rnn_beat_processor.py
@@ -1,7 +1,10 @@
 from os import path
 from pydub import AudioSegment
 from madmom.features.beats import RNNBeatProcessor
+import datetime
 
+print("starting conversion")
+start_time = datetime.datetime.now()
 input_file = "mp3_files/YQHsXMglC9A.mp3"
 output_file = "mp3_files/result.wav"
 
@@ -9,5 +12,8 @@ output_file = "mp3_files/result.wav"
 sound = AudioSegment.from_mp3(input_file)
 sound.export(output_file, format="wav")
 
+conversion_end_time = datetime.datetime.now()
+print("conversion took:", conversion_end_time - start_time)
 processor = RNNBeatProcessor()
 print(processor(output_file))
+print("beat processing took:", datetime.datetime.now() - conversion_end_time)
